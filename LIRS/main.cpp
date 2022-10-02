304b241026051231705627e2fe4b62d31fca2405
kseniadmitrievnadobrovolskaya <kseniadmitrievnadobrovolskaya@mail.ru>
2022-10-02 16:09:05 +0300

    change hits

--- LIRS/main.cpp
+++ LIRS/main.cpp
@@ -1,49 +1,25 @@
 #include "LIRS.h"
 
 
-int get_num(FILE *data);
-
 
 int main()
 {
-	FILE *data = fopen("012.dat", "r");
-	if (!data)
-	{
-		perror("fopen");
-		return 0;
-	}
-	
-	int len_cache = get_num(data),
-	count_nums = get_num(data);
+	int len_cache, count_nums;
+	std::cin >> len_cache >> count_nums;
 
 	struct Cash *LIRS = create_Cash(len_cache);
 
-	int hits = 0;
+	int elem, hits = 0;
 
 	for (int num = 0; num < count_nums; num++)
 	{
-		LIRS = append_elem(LIRS, get_num(data), &hits);
+		std::cin >> elem;
+		LIRS = append_elem(LIRS, elem, &hits);
 	}
-
-	fclose(data);
-
+	
 	destroy_cash(LIRS);
 
 	printf("For %d elems (size of cache %d) - %d hits\n", count_nums, len_cache, hits);
 
 	return 0;
-}
-
-
-int get_num(FILE *data)
-{
-	int elem;
-
-	if (!fscanf(data, "%d", &elem))
-	{
-		perror("fscanf");
-		_Exit(1);
-	}
-
-	return elem;
 }
